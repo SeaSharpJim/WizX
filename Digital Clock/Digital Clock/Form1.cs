dbeb7a406bc3bb9cbea1b99058f80ac3bb52d6be
SeaSharpJim <30943552+SeaSharpJim@users.noreply.github.com>
2017-08-11 22:17:21 -0400

    Digital Clock Gui
    Digital Clock in C# to convert time zones. A small sample project I have done.

---
+++ Digital Clock/Digital Clock/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Digital_Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Digital Clock"; //To set the title
+            timer1.Start(); //starting the timer
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //to display the time in the label
+            label1.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+    }
+}
